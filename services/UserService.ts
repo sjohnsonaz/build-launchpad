bf91118bc62af8245fb34f1240ef3497937249f9
Sean Johnson <sjohnson@sjohnsonaz.com>
2016-07-13 16:15:34 -0700

    Cleaned UserService.

--- services/UserService.ts
+++ services/UserService.ts
@@ -5,15 +5,4 @@ import AuthHelper from '../helpers/AuthHelper';
 import UserGateway from '../gateways/UserGateway';
 
 export default class UserService extends CrudService<UserGateway> {
-    @route('get', 'test/:id')
-    @middleware(AuthHelper.admin)
-    test(req, res, next) {
-        this.gateway.get(req.params.id, function(err, result) {
-            if (err) {
-                return next(err);
-            } else {
-                res.json(result);
-            }
-        });
-    }
 }
