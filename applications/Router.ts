175b06f17527af2a34c20e9bf56525ca32788309
Sean Johnson <sjohnson@sjohnsonaz.com>
2016-10-19 15:42:14 -0700

    Fixed route testing.

--- applications/Router.ts
+++ applications/Router.ts
@@ -1,49 +1,27 @@
-export interface IRoute {
-    route?: string;
-    enter: Function;
-    exit?: Function;
-    regex?: RegExp;
-}
-
-export interface IRouteIndex {
-    [index: string]: IRoute;
-}
-
-export interface IRouteQuickDefinition {
-    [index: string]: Function;
-}
-
 export default class Router {
-    static STRIP_COMMENTS = /((\/\/.*$)|(\/\*[\s\S]*?\*\/))/mg;
     routes: IRouteIndex = {};
     defaultRoutes: IRoute[] = [];
 
-    private getParameterNames(functionHandle: Function) {
-        var definition = functionHandle.toString().replace(Router.STRIP_COMMENTS, '');
-        return definition.slice(definition.indexOf('(') + 1, definition.indexOf(')')).match(/([^\s,]+)/g) || [];
-    }
-
-    constructor(routes?: IRouteQuickDefinition) {
-        if (routes) {
-            for (var index in routes) {
-                var entry = routes[index];
-                this.add(index, entry);
-            }
+    constructor(definitions?: IRouteQuickDefinition) {
+        if (definitions) {
+            this.addAll(definitions);
         }
     }
 
     handler = (event?: HashChangeEvent) => {
         var url = (event && event.newURL) ? event.newURL : window.location.href;
+        var hash = getHash(url);
         var routes = this.routes;
         var routesCalled = 0;
         for (var route in routes) {
             if (routes.hasOwnProperty(route)) {
                 var routeDef = routes[route];
-                var params = url.match(routeDef.regex);
+                var params = hash.match(routeDef.regex);
                 if (params) {
                     routesCalled++;
                     routeDef.enter.apply(routeDef.enter, params.slice(1));
-                    break;
+                    // TODO: Should this break after it has been called?
+                    //break;
                 }
             }
         }
@@ -64,28 +42,50 @@ export default class Router {
         window.removeEventListener('hashchange', this.handler);
     }
 
-    add(route: string, enter: Function, exit?: Function) {
+    addAll(definitions?: IRouteQuickDefinition) {
+        for (var index in definitions) {
+            var definition = definitions[index];
+            if (definition instanceof Array) {
+                var routes = buildRouteGroup(index, definition);
+                this.addRoutes(routes);
+            } else {
+                var route = buildRouteFromFunction(index, definition);
+                this.addRoute(route);
+            }
+        }
+    }
+
+    add(route: string, enter: Function) {
         if (route) {
             this.routes[route] = {
-                route: route,
+                name: route,
                 enter: enter,
-                exit: exit,
                 regex: new RegExp(route.replace(/\//g, "\\/").replace(/:(\w*)/g, "(\\w*)"))
             };
         } else {
             this.defaultRoutes.push({
-                enter: enter,
-                exit: exit
+                enter: enter
             });
         }
     }
 
-    remove(route) {
+    addRoute(route: IRoute) {
+        this.routes[route.name] = route;
+    }
+
+    addRoutes(routes: IRoute[]) {
+        for (var index = 0, length = routes.length; index < length; index++) {
+            var route = routes[index];
+            this.routes[route.name] = route;
+        }
+    }
+
+    remove(route: string) {
         delete this.routes[route];
     }
 
     watchMethod(scope, name, prefix, callback) {
-        var parameterNames = this.getParameterNames(callback);
+        var parameterNames = getParameterNames(callback);
         parameterNames.unshift(prefix);
         this.add('#/' + parameterNames.join('/:'), function() {
             callback.apply(scope, arguments);
@@ -109,4 +109,78 @@ export default class Router {
     go() {
 
     }
+}
+
+export interface IRoute {
+    name?: string;
+    enter: Function;
+    regex?: RegExp;
+}
+
+export interface IRouteIndex {
+    [index: string]: IRoute;
+}
+
+export interface IRouteQuickDefinition {
+    [index: string]: Function | Function[];
+}
+
+const STRIP_COMMENTS = /((\/\/.*$)|(\/\*[\s\S]*?\*\/))/mg;
+
+function getParameterNames(functionHandle: Function) {
+    var definition = functionHandle.toString().replace(STRIP_COMMENTS, '');
+    return definition.slice(definition.indexOf('(') + 1, definition.indexOf(')')).match(/([^\s,]+)/g) || [];
+}
+
+function buildRegexFromDefinition(definition: string) {
+    //    return new RegExp(definition.replace(/\//g, "\\/").replace(/:(\w*)/g, "(\\w*)"));
+    return new RegExp('^' + definition.replace(/\//g, '\\/').replace(/:(\w*)/g, '([\^S\^\/]*)') + '$');
+}
+
+function buildRouteFromDefinition(definition: string, enter: Function): IRoute {
+    return {
+        name: definition,
+        enter: enter,
+        regex: buildRegexFromDefinition(definition)
+    };
+}
+
+function buildRouteFromRegex(regex: RegExp, enter: Function): IRoute {
+    return {
+        name: regex.toString(),
+        enter: enter,
+        regex: regex
+    };
+}
+
+function buildRouteFromFunction(prefix: string, enter: Function): IRoute {
+    var params = getParameterNames(enter);
+    params.unshift(prefix);
+    var definition = params.join('/:');
+    var regex = buildRegexFromDefinition(definition);
+    return {
+        name: definition,
+        enter: enter,
+        regex: regex
+    };
+}
+
+function buildRouteGroup(prefix: string, entries: Function[]): IRoute[] {
+    var routes: IRoute[] = [];
+    for (var index = 0, length = entries.length; index < length; index++) {
+        var entry = entries[index];
+        routes.push(buildRouteFromFunction(prefix, entry));
+    }
+    return routes;
+}
+
+function getHash(url: string) {
+    var hash;
+    var index = url.indexOf('#');
+    if (index >= 0) {
+        hash = url.substring(index + 1);
+    } else {
+        hash = '';
+    }
+    return hash;
 }
--- applications/Routing.ts
+++ applications/Routing.ts
@@ -4,12 +4,14 @@ import Router from './Router';
 export default class Routing {
     static run() {
         var router = new Router({
-            'user/:id/:name': function(id, name) {
-                console.log('Route \'user/:id/:name\' ' + id + ', ' + name);
-            },
-            'user/:id': function(id) {
-                console.log('Route \'user/:id\' ' + id);
-            },
+            'user': [
+                function(id, name) {
+                    console.log('Route \'user/:id/:name\' ' + id + ', ' + name);
+                },
+                function(id) {
+                    console.log('Route \'user/:id\' ' + id);
+                }
+            ],
             '': function() {
                 console.log('Route \'\'');
             }
